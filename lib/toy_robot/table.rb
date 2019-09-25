92d80700aa613dbdd15df6966c87b593b75709e6
LevPewPew <levtoth1988@gmail.com>
2019-09-26 09:23:59 +1000

    whitespace

--- lib/toy_robot/table.rb
+++ lib/toy_robot/table.rb
@@ -9,7 +9,7 @@ module ToyRobot
 
     def valid_position?(east, north)
       if east >= 0 && east <= 5 && north >= 0 && north <= 5
-          return true
+        return true
       else
         return false
       end
